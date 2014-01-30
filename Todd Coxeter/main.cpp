e59b3182d89519eb0d386600ff79106622e10bcb
XuXo <tianxing.xu@gmail.com>
2014-01-30 18:45:50 -0500

    Initial Commit

---
+++ Todd Coxeter/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Todd Coxeter
+//
+//  Created by XuXo on 1/30/14.
+//  Copyright (c) 2014 XuXo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
